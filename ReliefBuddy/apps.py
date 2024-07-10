06eecf822dde91e9103a4f0f7fc05ad6f48dc381
Pyae Linn <150521896+PyaeLinn01@users.noreply.github.com>
2024-07-10 08:42:59 +0000

    6

--- ReliefBuddy/apps.py
+++ ReliefBuddy/apps.py
@@ -70,5 +70,5 @@ def get_response(intents_list, intents_json):
     return result
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=6000, debug=True)
 
