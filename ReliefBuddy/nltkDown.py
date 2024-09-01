8f95950390f1163a9f1ed8c97cbd76e886882192
Pyae Linn <150521896+PyaeLinn01@users.noreply.github.com>
2024-09-01 05:43:29 +0000

    modifyNLTK

--- ReliefBuddy/nltkDown.py
+++ ReliefBuddy/nltkDown.py
@@ -1,7 +1,19 @@
 import nltk
+import os
 
+# Set the path for nltk data
+nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
+
+if not os.path.exists(nltk_data_dir):
+    os.makedirs(nltk_data_dir)
+
+# Add the nltk data directory to nltk's data path
+nltk.data.path.append(nltk_data_dir)
+nltk.download('punkt_tab')
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
 
 print("Punkt tokenizer downloaded successfully.")
